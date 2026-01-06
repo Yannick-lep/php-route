fb3a331725982b07f86453839020dbb8fd22cb64
Yannick Lepin <lepinayyannick@gmail.com>
2026-01-06 11:22:41 +0100

    filtre sur la variable page

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 include 'functions.php';
+require 'routes.php';
 
-$page = $_GET['page'] ?? 'home';
-
-$routes = [
-    'home' => 'home.php',
-    'contact' => 'contact.php',
-    'about' => 'about.php'
-];
+//$page =isset($GET['page]) ? $_GET['page] : 'home';
+//$page = $_GET['page'] ?? 'home';
+$pageFiltre = filter input(INPUT GET, 'page', FILTER SANITIZE FULL SPECIAL CHARS);
+$page = $pageFiltre ?? 'home';
 
 if (!array_key_exists($page,$routes)){
    redirect('404.php');
